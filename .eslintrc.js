c9f800ea2de9341f4ce6fb6bf247a8e4bfe28da9
唐道海 <tangdaohai@outlook.com>
2020-11-14 00:32:25 +0800

    chore(project): config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'plugin:jest/all'
+  ]
+}
